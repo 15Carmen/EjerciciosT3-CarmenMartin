857722f4e7d3aec6383a3cb9862a727e76612ded
cmartin <carmen.martin@iesnervion.es>
2022-11-14 10:14:47 +0100

    commit4

---
+++ src/Ejercicio10.java
@@ -0,0 +1,8 @@
+public class Ejercicio10 {
+    /*
+    Realiza un programa que nos diga si un número introducido por teclado es capicúa o no.
+     */
+    public static void main(String[] args) {
+
+    }
+}
