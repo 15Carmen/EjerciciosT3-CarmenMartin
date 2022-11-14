8f7305129fe445fdb0604fa092626a38a115c062
15Carmen <carmen.martin@iesnervion.es>
2022-11-14 22:20:28 +0100

    commit5

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,6 +1,8 @@
 public class Ejercicio5 {
     /*
-
+    De forma similar a la actividad 4, realiza un programa que calcule el mínimo común múltiplo de dos números dados.
+    En este caso, habrá que partir del máximo de los dos e ir incrementando hasta encontrar el primer número que sea
+    múltiplo de los dos números.
      */
     public static void main(String[] args) {
 
--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,6 +1,12 @@
 public class Ejercicio6 {
     /*
-
+    Realiza un programa que pida un número entero N entre 0 y 20 y luego muestre por pantalla los números desde 1
+    hasta N, uno en cada línea, repitiendo cada número tantas veces como su valor. Ejemplo:
+    1
+    22
+    333
+    4444
+    55555
      */
     public static void main(String[] args) {
 
